200a7c33d9e5f69e29f228cce0e0f6e123643f86
ricardo <fkondem@gmail.com>
2025-01-14 08:27:08 +0100

    added db connection

--- sequelize.js
+++ sequelize.js
@@ -3,9 +3,10 @@ const { Sequelize } = require('sequelize');  // Ensure Sequelize is imported
 
 const environment = 'development'; // Change this if using a different environment
 
-const sequelize = new Sequelize('drollon', 'postgres', 'san123tos', {
-  host: 'localhost',
-  dialect: 'postgres'
+
+const sequelize = new Sequelize(`postgresql://neondb_owner:HIFuL4jt0pvG@ep-tiny-base-a5rexjlz.us-east-2.aws.neon.tech/neondb?sslmode=require
+`, {
+  dialect: dbConfig.dialect,
 });
 
 // Test the connection
